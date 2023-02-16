f5d5335cbd602bcd7b15efb70b1fa67af4347e3b
TosheTeov <76562841+Toshe10@users.noreply.github.com>
2023-02-16 02:45:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function (){
+    let weather = {
+        "apiKey":"a597622460e3cf9ae1472889b2fb3086",
+        fetchWeather: function(city){
+                  fetch(
+                  "https://api.openweathermap.org/data/2.5/weather?q="
+                   + city 
+                   +"&units=metric&appid="
+                   + this.apiKey
+                   ).then((response) => response.json()).then((data) => this.displayWeather(data));
+        },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed" + speed + "km/h";
+        
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+        console.log(document.querySelector(".search-bar").value)
+}
+};
+document.querySelector("button").addEventListener("click", function() {
+    weather.search();
+
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+
+    }
+})
+
+})
+// weather.fetchWeather("Veles");
